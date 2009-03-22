80cc1b491d29e9885ca73fb0e57d5f5bfc02a624
David <David@daviddesktop>
2009-03-22 14:47:36 +1300

    Added a strongname requirement to the assembly. If you wish to build this, make a strong name key file of your own called "SQLyte.pfx" and then sign the assembly with it.

--- SQLiteBrowser/Properties/AssemblyInfo.cs
+++ SQLiteBrowser/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLiteBrowser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SQLyte")]
+[assembly: AssemblyDescription("An SQLite Browser written in .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(".")]
+[assembly: AssemblyCompany("BunnyBusiness")]
 [assembly: AssemblyProduct("SQLiteBrowser")]
-[assembly: AssemblyCopyright("Copyright © . 2009")]
+[assembly: AssemblyCopyright("Copyright © astrobunny. 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
